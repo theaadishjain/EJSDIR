6296a9f2503689c0a833fe6da4afa4eeef4a4254
theaadishjain <theaadishjain@gmail.com>
2025-02-20 07:39:46 +0530

    instagram template basic

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.set("view engine","ejs");
 app.get("/",(req,res)=>{
     res.render("home.ejs");
 });
+
+app.get("/ig/:username",(req,res)=>{
+   let {username}= req.params;
+    res.render("instagram.ejs",{username});
+});
+
+
 app.get("/hello",(req,res)=>{
     res.send("home");
 });
